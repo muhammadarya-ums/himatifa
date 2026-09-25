71694effa812bb1eb78bc70a1af71f4902009a38
muhammadarya-ums <muhammad.arya.putra-2024@ft.um-surabaya.ac.id>
2026-09-25 21:08:48 +0700

    again

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,19 +20,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/himatifabg.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/himatifabg.png',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: '/himatifabg.png',
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/himatifabg.png',
   },
 
   openGraph: {
